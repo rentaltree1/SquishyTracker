1042a557ad4ccbee7ce176b0fd99db4b0a702f2c
rentaltree1 <rentaltree5@gmail.com>
2026-08-11 21:09:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 import threading
 from bot import run_discord_bot
 
 app = Flask(__name__)
 
-# We moved this up here so Render actually runs it!
+# Start the Discord bot in a background thread
 bot_thread = threading.Thread(target=run_discord_bot)
 bot_thread.start()
 
+# This tells Flask to load your shiny new UI from the templates folder!
 @app.route('/')
 def home():
-    return """
-    <h1>Squishy Tracker Dashboard</h1>
-    <p>The Discord bot is running successfully in the background!</p>
-    """
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
